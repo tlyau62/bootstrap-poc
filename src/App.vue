fddd12312d1313a85cab5b6ba5c81b9c9a5f9e2c
Tim Yau <tyautl@nexify.com.hk>
2020-03-13 12:52:40 +0800

    Add scss bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+
+    <button type="button" class="btn btn-red">Primary</button>
+
+    <router-view />
   </div>
 </template>
+
+<style src="./App.scss" lang="scss"></style>
 
 <style lang="scss">
 #app {
